aadf20d9526206fc329067ded3cfe8efb319e855
Admin <kolkataradmobile13@gmail.com>
2018-05-08 11:08:15 +0530

    Added new tests

--- app/src/test/java/bestapps/assesmentwipror/ResponseTest.java
+++ app/src/test/java/bestapps/assesmentwipror/ResponseTest.java
@@ -1,9 +1,9 @@
 package bestapps.assesmentwipror;
-
 import org.junit.Test;
-
+import org.mockito.Mock;
 import java.io.IOException;
-
+import java.util.List;
+import bestapps.assesmentwipror.model.Row;
 import bestapps.assesmentwipror.model.Wipro;
 import bestapps.assesmentwipror.remote.RetrofitClient;
 import bestapps.assesmentwipror.interfaces.WipServiceInterface;
@@ -11,6 +11,7 @@ import retrofit2.Call;
 import retrofit2.Response;
 
 import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -18,6 +19,9 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ResponseTest {
+    @Mock
+    Row row;
+
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
@@ -38,6 +42,44 @@ public class ResponseTest {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+    /**
+     * Test to validate Row Count
+     */
+    @Test
+    public void rowsCountCheck() {
+        List<Row> mRows;
+        WipServiceInterface apiEndpoints = RetrofitClient.getClient().create(WipServiceInterface.class);
+        Call<Wipro> call = apiEndpoints.getServerData();
+        try {
+            Response<Wipro> response = call.execute();
+            Wipro authResponse = response.body();
+            mRows = authResponse.getRows();
+            if (!mRows.isEmpty()) {
+                assertEquals(14, authResponse.getRows().size());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
+    /**
+     * Test to validate row Description
+     */
+    @Test
+    public void rowDescCheck() {
+        List<Row> mRows;
+        WipServiceInterface apiEndpoints = RetrofitClient.getClient().create(WipServiceInterface.class);
+        Call<Wipro> call = apiEndpoints.getServerData();
+        try {
+            Response<Wipro> response = call.execute();
+            Wipro authResponse = response.body();
+            mRows = authResponse.getRows();
+            row = mRows.get(10);
+            assertEquals("It's really big.", row.getDescription());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
